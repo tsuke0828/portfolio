7a1e6ef79c330b144495079b20d7f4ed3e62c488
tsuke0828 <107971852+tsuke0828@users.noreply.github.com>
2022-07-23 11:09:06 +0900

    mozikeus

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
 $('.imgslid').slick({
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 1,//スライドを画面に3枚見せる
-    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-    dots: true,//下部ドットナビゲーションの表示
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true,
     
 });
 
 lightbox.option({
-    'wrapAround': true,//グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
-    'albumLabel': ' %1 / total %2 '//合計枚数中現在何枚目かというキャプションの見せ方を変更できる
+    'wrapAround': true,
+    'albumLabel': ' %1 / total %2 '
   })
